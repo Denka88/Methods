868434b8e866d4d8ce258f526654ed79d0336a9f
Denka88 <shadowfightdenka88@gmail.com>
2024-10-08 15:56:23 +0300

    Задание 1, Класс main() и класс triangle()

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Введите сторону а:");
+        int a = scan.nextInt();
+        System.out.print("Введите сторону b:");
+        int b = scan.nextInt();
+        System.out.print("Введите сторону c:");
+        int c = scan.nextInt();
+
+        triangle(a, b, c);
+
+    }
+
+    public static void triangle(int a, int b, int c){
+
+        if(a + b > c && a + c > b && b + c > a){
+            System.out.println("Такой треугольник существует");
+        }else {
+            System.out.println("Такого треугольника не существует");
+        }
 
 
     }
+
 }
